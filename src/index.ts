00fce78846756ac58e73f283bb353c4592916b19
Sebastian Vega <svegalopez@gmail.com>
2019-11-16 04:34:44 +0000

    completed io streams

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,73 @@
 import { createReadStream } from 'fs';
-import { Writable, Transform } from 'stream';
+import { Writable, WritableOptions } from 'stream';
 const csv = require('fast-csv');
 
-const rs = createReadStream('./data.csv');
+type Story = {
+    launchDate: string,
+    title: string,
+    privacy: 'public' | 'private',
+    likes: number
+}
 
-const x: Transform = rs.pipe(csv.parse({ headers: true }))
+let rows: Story[] = [];
 
-const outStream = new Writable({
-    write(chunk, encoding, callback) {
-        writeToDB(chunk).then(_ => callback())
-    },
-    objectMode: true
-});
+const write = (row: Story, enc: string, cb: (err?: Error | null) => void): void => {
+    rows.push(row);
+    if (rows.length === 100000) {
+        writeToDB(rows)
+            .then(() => {
+                rows = []
+                cb()
+            })
+            .catch((err: Error) => {
+                cb(err)
+            })
+    } else {
+        cb()
+    }
+}
 
-//x.pipe(outStream)
+const readStream = createReadStream('./stories.csv')
+const transformStream = readStream.pipe(csv.parse({ headers: true }))
+const writeStream = new Writable({ write, objectMode: true })
 
-x.on('data', (row: any) => {
-    outStream.write(row)
+transformStream.pipe(writeStream).on('finish', () => {
+    if (rows.length > 0) {
+        writeToDB(rows)
+            .then(() => {
+                rows = [];
+                console.log('done')
+            })
+            .catch((err: Error) => {
+                console.log(err)
+            })
+    }
+})
+
+function writeToDB(rows: Story[]): Promise<void> {
+    return new Promise((res, rej) => {
+        setTimeout(() => {
+            console.log(rows)
+            res()
+        }, 100)
+    })
+}
+
+writeStream.on('error', (err: Error) => {
+    console.log('There was an error writing: ')
+    console.log(err)
+})
+
+readStream.on('error', (err: Error) => {
+    console.log('There was an error reading: ')
+    console.error(err)
+})
+
+transformStream.on('error', (err: Error) => {
+    console.log('There was an error transforming: ')
+    console.error(err)
 })
 
 
-function writeToDB(row: any): Promise<void> {
-    return new Promise(res => setTimeout(() => {
-        console.log(row);
-        res()
-    }, 100))
-}
+
+
